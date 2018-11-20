5818e6f25a8d745570f39874da979fc941089548
Mitchel Hulst <mitchelhulst@gmail.com>
2018-11-20 17:04:10 +0100

    default disable stuff

--- JavaScript/array-methods.js
+++ JavaScript/array-methods.js
@@ -6,125 +6,125 @@ $('document').ready(function(){
     // Length method
     console.log('Length of names array: ' + names.length);
 
-    // Concat method
-    separator();
-    var names_concat = new Array("Piet");
-    console.log('Added another array to existing array names: ' + names.concat(names_concat));
+    // // Concat method
+    // separator();
+    // var names_concat = new Array("Piet");
+    // console.log('Added another array to existing array names: ' + names.concat(names_concat));
     
-    // IndexOf
-    separator();
-    console.log("Karel is at place 1 in the names array: " + names.indexOf("Karel", 0));
+    // // IndexOf
+    // separator();
+    // console.log("Karel is at place 1 in the names array: " + names.indexOf("Karel", 0));
     
-    // Join
-    separator();
-    console.log("Join with '\\': " + names.join("\\"));
-    console.log("Join with ',': " + names.join(","));
-    console.log("Join with '|': " + names.join("|"));
+    // // Join
+    // separator();
+    // console.log("Join with '\\': " + names.join("\\"));
+    // console.log("Join with ',': " + names.join(","));
+    // console.log("Join with '|': " + names.join("|"));
         
-    // Reverse
-    separator();
-    console.log(JSON.stringify(names.reverse()));
+    // // Reverse
+    // separator();
+    // console.log(JSON.stringify(names.reverse()));
     
-    // Slice
-    separator();
-    const sliced = names.slice(1,3);
-    console.log("Sliced from the names array: " + sliced);
+    // // Slice
+    // separator();
+    // const sliced = names.slice(1,3);
+    // console.log("Sliced from the names array: " + sliced);
     
-    // Splice
-    separator();
-    var names_splice = new Array("John", "Karel", "Kees", "Robert", "Trudie");
-    var spliced = names_splice.splice(1, 3);
-    var names_splice_and_replace = new Array("John", "Karel", "Kees", "Robert", "Trudie");
-    var spliced_and_replace = names_splice_and_replace.splice(1, 3, "Pieter", "Piet", "Klaas");
+    // // Splice
+    // separator();
+    // var names_splice = new Array("John", "Karel", "Kees", "Robert", "Trudie");
+    // var spliced = names_splice.splice(1, 3);
+    // var names_splice_and_replace = new Array("John", "Karel", "Kees", "Robert", "Trudie");
+    // var spliced_and_replace = names_splice_and_replace.splice(1, 3, "Pieter", "Piet", "Klaas");
 
-    console.log("Splice only: " + spliced);
-    console.log("Spliced items are replaces with other names: " + names_splice_and_replace);
+    // console.log("Splice only: " + spliced);
+    // console.log("Spliced items are replaces with other names: " + names_splice_and_replace);
     
-    // Pop and Push (FIFO)
-    separator();
-    const pop_push_array = new Array();
-    pop_push_array.push('This', 'is', 'pushed');
+    // // Pop and Push (FIFO)
+    // separator();
+    // const pop_push_array = new Array();
+    // pop_push_array.push('This', 'is', 'pushed');
 
-    console.log("When push(...) is used: " + pop_push_array);
-    console.log("When pop() (pop last item in array) is used: " + pop_push_array.pop());
-    console.log("After pop() is used: " + pop_push_array);
+    // console.log("When push(...) is used: " + pop_push_array);
+    // console.log("When pop() (pop last item in array) is used: " + pop_push_array.pop());
+    // console.log("After pop() is used: " + pop_push_array);
     
-    // Shift and Unshift
-    separator();
-    const shift_unshift_array = new Array();
-    shift_unshift_array.unshift('This', 'is', 'unshifted');
+    // // Shift and Unshift
+    // separator();
+    // const shift_unshift_array = new Array();
+    // shift_unshift_array.unshift('This', 'is', 'unshifted');
 
-    console.log("When unshift(...) is used: " + shift_unshift_array);
-    console.log("When shift() (shift first item in array) is used: " + shift_unshift_array.shift());
-    console.log("After shift() is used: " + shift_unshift_array);    
+    // console.log("When unshift(...) is used: " + shift_unshift_array);
+    // console.log("When shift() (shift first item in array) is used: " + shift_unshift_array.shift());
+    // console.log("After shift() is used: " + shift_unshift_array);    
 
-    // Every (Callback)
-    separator();
-    const evenNumbers = new Array(0, 2, 4, 6, 8, 9);
+    // // Every (Callback)
+    // separator();
+    // const evenNumbers = new Array(0, 2, 4, 6, 8, 9);
     
-    var allEven = evenNumbers.every((value, index, array) => {
-        return value % 2 === 0;
-    });
+    // var allEven = evenNumbers.every((value, index, array) => {
+    //     return value % 2 === 0;
+    // });
 
-    console.log(evenNumbers);
-    console.log("Are all the numbers even?: " + allEven);
+    // console.log(evenNumbers);
+    // console.log("Are all the numbers even?: " + allEven);
 
-    // Some (Callback)
-    separator();
-    const some_numbers = new Array(0, 2, 4, 6, 8, 9);
+    // // Some (Callback)
+    // separator();
+    // const some_numbers = new Array(0, 2, 4, 6, 8, 9);
 
-    var are_some_uneven = some_numbers.some((value, index, array) => {
-        return value % 2 !== 0;
-    });
+    // var are_some_uneven = some_numbers.some((value, index, array) => {
+    //     return value % 2 !== 0;
+    // });
 
-    console.log(some_numbers);
-    console.log("Are some numbers uneven?: " + are_some_uneven);
+    // console.log(some_numbers);
+    // console.log("Are some numbers uneven?: " + are_some_uneven);
 
-    // forEach (Callback)
-    separator();
-    const foreach_numbers = new Array(0, 2, 4, 6, 8, 9);
-    var multiplied_array = new Array();
+    // // forEach (Callback)
+    // separator();
+    // const foreach_numbers = new Array(0, 2, 4, 6, 8, 9);
+    // var multiplied_array = new Array();
 
-    foreach_numbers.forEach((value, index, array) => {
-        multiplied_array.push(value * 2);
-    })
+    // foreach_numbers.forEach((value, index, array) => {
+    //     multiplied_array.push(value * 2);
+    // })
 
-    console.log(foreach_numbers);
-    console.log("Multiplied: " + multiplied_array);
+    // console.log(foreach_numbers);
+    // console.log("Multiplied: " + multiplied_array);
 
-    // Filter (Callback)
-    separator();
-    const filter_numbers = new Array(0, 6, 8, 18, 17, 14, 99, 95, 1005);
-    const even_numbers_array = new Array();
+    // // Filter (Callback)
+    // separator();
+    // const filter_numbers = new Array(0, 6, 8, 18, 17, 14, 99, 95, 1005);
+    // const even_numbers_array = new Array();
     
-    filter_numbers.filter((value, index, array) => {
-        if (value % 2 === 0) {
-            even_numbers_array.push(value);
-        }
-    });
+    // filter_numbers.filter((value, index, array) => {
+    //     if (value % 2 === 0) {
+    //         even_numbers_array.push(value);
+    //     }
+    // });
 
-    console.log(filter_numbers);
-    console.log("All even numbers are filtered: " + even_numbers_array);
+    // console.log(filter_numbers);
+    // console.log("All even numbers are filtered: " + even_numbers_array);
 
-    // Map (Callback)
-    separator();
-    const map_numbers = new Array(1.2, 2.6, 3.7, 4.1, 5.9);
-    console.log(map_numbers);
+    // // Map (Callback)
+    // separator();
+    // const map_numbers = new Array(1.2, 2.6, 3.7, 4.1, 5.9);
+    // console.log(map_numbers);
 
-    const rounded = map_numbers.map((value, index, array) => {
-        return Math.round(value);
-    });
+    // const rounded = map_numbers.map((value, index, array) => {
+    //     return Math.round(value);
+    // });
 
-    console.log("Values are replaced: " + rounded);
+    // console.log("Values are replaced: " + rounded);
 
-    // Reduce and ReduceRight
-    separator();
-    const reduce_numbers = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    // // Reduce and ReduceRight
+    // separator();
+    // const reduce_numbers = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-    var factorial = reduce_numbers.reduce((previous, current) => {
-        return previous * current;
-    });
+    // var factorial = reduce_numbers.reduce((previous, current) => {
+    //     return previous * current;
+    // });
 
-    console.log(reduce_numbers);
-    console.log("Reduce used: " + factorial);
+    // console.log(reduce_numbers);
+    // console.log("Reduce used: " + factorial);
 });
--- JavaScript/objects.js
+++ JavaScript/objects.js
@@ -26,26 +26,26 @@ function Book(title, length, author) {
   this.author = author;
 }
 
-Book.prototype = {
-  ISBN: '',
-  Length: -1,
-  genre: '',
-  covering: '',
-  author: '',
-  currentPage: 0,
-  title: '',
+// Book.prototype = {
+//   ISBN: '',
+//   Length: -1,
+//   genre: '',
+//   covering: '',
+//   author: '',
+//   currentPage: 0,
+//   title: '',
 
-  flipTo: function FlipToAPage(pNum) {
-    this.currentPage = pNum;
-  },
+//   flipTo: function FlipToAPage(pNum) {
+//     this.currentPage = pNum;
+//   },
 
-  turnPageForward: function turnForward() {
-    this.flipTo(this.currentPage++);
-  },
+//   turnPageForward: function turnForward() {
+//     this.flipTo(this.currentPage++);
+//   },
 
-  turnPageBackward: function turnBackward() {
-    this.flipTo(this.currentPage--);
-  }
-};
+//   turnPageBackward: function turnBackward() {
+//     this.flipTo(this.currentPage--);
+//   }
+// };
 
 var books = new Array(new Book(), new Book('First Edition', 350, 'Random'));
